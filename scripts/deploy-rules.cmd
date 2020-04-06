d2ebdbbf1145600fd293e1874944cc244ff53b95
Andrew Hall <andrew.hall@uipath.com>
2020-04-06 15:21:27 -0700

    cmd to deploy sample rules

---
+++ scripts/deploy-rules.cmd
@@ -0,0 +1,26 @@
+echo off
+REM look for community folder
+FOR /D %%G in ("%localappdata%\UiPath\app-*") DO set COMMUNITY_INSTALL_FOLDER=%%G
+if exist "%COMMUNITY_INSTALL_FOLDER%\Rules\" (
+    SET RULES_FOLDER="%COMMUNITY_INSTALL_FOLDER%\Rules\"
+    goto copyrules
+) 
+if exist "%programfiles(x86)%\UiPath\Studio\Rules" (
+    SET RULES_FOLDER="%programfiles(x86)%\UiPath\Studio\Rules"
+    goto copyrules
+)
+else (
+    echo "No Studio installation found"
+    goto commonexit
+)
+
+:copyrules
+if exist %RULES_FOLDER% (
+    set SAMPLE_BINARY_DIR=..\sample-rule-binaries
+    echo on
+    for /f %%f in ('dir /b %SAMPLE_BINARY_DIR%') DO copy /Y %SAMPLE_BINARY_DIR%\%%f %RULES_FOLDER%
+    echo off
+    goto commonexit
+)
+
+:commonexit
