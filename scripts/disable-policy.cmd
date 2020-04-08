8c050adf9df8436e79db70af9a16de34ee5b10c8
Andrew Hall <andrew.hall@uipath.com>
2020-04-08 16:23:34 -0700

    updates to studio policy

--- scripts/disable-policy.cmd
+++ scripts/disable-policy.cmd
@@ -1,1 +1,5 @@
 reg  delete  HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource /f
+
+if exist "%appdata%\UiPath\uipath.policies.config.cache"(
+    del "%appdata%\UiPath\uipath.policies.config.cache"
+)
