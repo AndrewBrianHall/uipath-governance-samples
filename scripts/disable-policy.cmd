39c0ed5b5d24b66c175d2b57474f3f70462b184a
Andrew Hall <andrew.hall@uipath.com>
2020-04-09 13:31:03 -0700

    updating enable/disable scripts

--- scripts/disable-policy.cmd
+++ scripts/disable-policy.cmd
@@ -1,28 +1,33 @@
-reg  delete  HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource /f
+@echo off
+
+reg query HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource >nul 
+if %errorlevel% == 0 (
+    reg  delete  HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource /f >nul
+)
 
 if exist "%appdata%\UiPath\uipath.policies.config.cache" (
     del "%appdata%\UiPath\uipath.policies.config.cache"
 )
 
-REM echo off
-REM look for community folder
 FOR /D %%G in ("%localappdata%\UiPath\app-*") DO set COMMUNITY_INSTALL_FOLDER=%%G
 if exist "%COMMUNITY_INSTALL_FOLDER%\Rules\" (
-    SET RULES_FOLDER=%COMMUNITY_INSTALL_FOLDER%\Rules\
-    goto deleterules
+    SET CUSTOM_RULE_FILE="%COMMUNITY_INSTALL_FOLDER%\Rules"
+    goto copyrules
 ) 
 if exist "%programfiles(x86)%\UiPath\Studio\Rules\" (
-    SET RULES_FOLDER=%programfiles(x86)%\UiPath\Studio\Rules
-    goto deleterules
-)
-
-:deleterules
-set SAMPLE_BINARY_DIR=%CD%\..\sample-rule-binaries
-if exist "%RULES_FOLDER%" (
-    echo on
-    for /f %%f in ('dir /b "%SAMPLE_BINARY_DIR%"') DO if exist "%RULES_FOLDER%\%%f" (del "%RULES_FOLDER%\%%f")
-    echo off
+    SET CUSTOM_RULE_FILE="%programfiles(x86)%\UiPath\Studio\Rules"
+    goto copyrules
+) else (
+    echo "No Studio installation found"
     goto commonexit
 )
 
+:copyrules
+set SAMPLE_BINARY_DIR=%CD%\..\sample-rule-binaries
+for /f %%f in ('dir /b "%SAMPLE_BINARY_DIR%"') DO if exist %CUSTOM_RULE_FILE%\%%f ( del %CUSTOM_RULE_FILE%\%%f )
+echo Custom rules deleted successfully
+goto commonexit
+
+
 :commonexit
+echo Policy successfully disabled
--- scripts/enable-policy.cmd
+++ scripts/enable-policy.cmd
@@ -1,10 +1,33 @@
-echo off
+@echo off
 if /I "%1" == "local" (
     SET POLICY_FILE="%CD%\..\sample-policy\uipath.policies.config"
 ) else (
     SET POLICY_FILE="https://raw.githubusercontent.com/AndrewBrianHall/uipath-governance-samples/master/sample-policy/uipath.policies.config"
 )
 
-echo on
+reg  add  HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource /t REG_SZ /f /d %POLICY_FILE% >nul
 
-reg  add  HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource /t REG_SZ /f /d %POLICY_FILE%
+FOR /D %%G in ("%localappdata%\UiPath\app-*") DO set COMMUNITY_INSTALL_FOLDER=%%G
+if exist "%COMMUNITY_INSTALL_FOLDER%\Rules\" (
+    SET RULES_FOLDER="%COMMUNITY_INSTALL_FOLDER%\Rules\"
+    goto copyrules
+) 
+if exist "%programfiles(x86)%\UiPath\Studio\Rules\" (
+    SET RULES_FOLDER="%programfiles(x86)%\UiPath\Studio\Rules"
+    goto copyrules
+) else (
+    echo "No Studio installation found"
+    goto commonexit
+)
+
+:copyrules
+set SAMPLE_BINARY_DIR=%CD%\..\sample-rule-binaries
+if exist %RULES_FOLDER% (
+    for /f %%f in ('dir /b "%SAMPLE_BINARY_DIR%"') DO copy /Y "%SAMPLE_BINARY_DIR%\%%f" %RULES_FOLDER% >nul
+    echo Custom rules deployed successfully
+    goto commonexit
+)
+
+:commonexit
+
+echo Policy successfully enabled
