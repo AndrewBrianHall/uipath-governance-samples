e94194836744efadd5208d91456ad3eb08bc6d3b
Andrew Hall <andrew.hall@uipath.com>
2020-04-09 13:34:15 -0700

    don't set policy key if no studio found

--- scripts/enable-policy.cmd
+++ scripts/enable-policy.cmd
@@ -5,29 +5,29 @@ if /I "%1" == "local" (
     SET POLICY_FILE="https://raw.githubusercontent.com/AndrewBrianHall/uipath-governance-samples/master/sample-policy/uipath.policies.config"
 )
 
-reg  add  HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource /t REG_SZ /f /d %POLICY_FILE% >nul
-
 FOR /D %%G in ("%localappdata%\UiPath\app-*") DO set COMMUNITY_INSTALL_FOLDER=%%G
 if exist "%COMMUNITY_INSTALL_FOLDER%\Rules\" (
     SET RULES_FOLDER="%COMMUNITY_INSTALL_FOLDER%\Rules\"
-    goto copyrules
+    goto enablepolicy
 ) 
 if exist "%programfiles(x86)%\UiPath\Studio\Rules\" (
     SET RULES_FOLDER="%programfiles(x86)%\UiPath\Studio\Rules"
-    goto copyrules
+    goto enablepolicy
 ) else (
     echo "No Studio installation found"
     goto commonexit
 )
 
-:copyrules
+:enablepolicy
+
+reg  add  HKEY_CURRENT_USER\Software\UiPath /v GovernanceSource /t REG_SZ /f /d %POLICY_FILE% >nul
+
 set SAMPLE_BINARY_DIR=%CD%\..\sample-rule-binaries
 if exist %RULES_FOLDER% (
     for /f %%f in ('dir /b "%SAMPLE_BINARY_DIR%"') DO copy /Y "%SAMPLE_BINARY_DIR%\%%f" %RULES_FOLDER% >nul
     echo Custom rules deployed successfully
-    goto commonexit
 )
 
-:commonexit
-
 echo Policy successfully enabled
+
+:commonexit
