711fdc84631412aa3e6ab17f21b587806897cc69
Andrew Hall <andrew.hall@uipath.com>
2020-04-10 07:39:12 -0700

    moving rule source to a different repo

--- src/SampleGovernanceRules/Models/PackageSettingsEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SampleGovernanceRules.Models
-{
-    internal class PackageSettingsEntry
-    {
-        public string Package { get; set; }
-        public string Setting { get; set; }
-        public string MinValue { get; set; }
-    }
-}
